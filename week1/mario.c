e8df9390a47a7634a20a761075bd8667f40928d5
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-27 15:16:41 +0200

    feat: file mario.c

---
+++ week1/mario.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_row(int n);
+
+int main(void){
+
+    // Never change n
+    const int n = 4;
+
+    for(int i = 0; i < n; i++){
+        print_row(n);
+    }
+}
+
+void print_row(int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
