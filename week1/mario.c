99ec865d2779e87ba7c5f3f7042edfad54fe0466
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-28 18:59:10 +0200

    add loop_for to printf spaces

--- week1/mario.c
+++ week1/mario.c
@@ -1,22 +1,26 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-void print_row(int n);
+void print_space(int height);
 
-int main(void){
-
-    // Never change n
-    const int n = 4;
-
-    for(int i = 0; i < n; i++){
-        print_row(n);
-    }
-}
-
-void print_row(int n){
-    for (int i = 0; i < n; i++)
-    {
-        printf("#");
-    }
-    printf("\n");
+int main(void)
+{
+    // Prompt user input
+    int height;
+        do
+        {
+            height = get_int("What is the height of the pyramid? ");
+        } while (height < 1);
+        
+    // Print a pyramid of that height
+        for (int i = 0; i < height; i++)
+        {
+            // Space 
+            for(int j = 1; j < height - i; j++)
+            {
+                printf("-");
+            }
+            printf("\n");
+        }
+        
 }
