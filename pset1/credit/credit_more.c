33222b7eb41aa3a7a1f6d6baf3141a81f5962b92
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-30 21:25:54 +0200

    Feat input validation loop for credit card number

--- pset1/credit/credit_more.c
+++ pset1/credit/credit_more.c
@@ -12,18 +12,28 @@ int main(void)
     }
     while (number < 1);
 
-#include <cs50.h>
-#include <stdio.h>
+    // Luhn algorithm
 
-int main(void)
-{
-    // Prompt user input 
-    long number;
+    int sum1 = 0;
+    int sum2 = 0;
+    int position = 0;
+    long temp = number;
 
-    do 
+    while (temp > 0)
     {
-        number = get_long("Card number: ");
-    }
-    while (number < 1);
+        int digit = temp % 10;
 
+        if (position % 2 == 0)
+        {
+            sum1 += digit;
+        }
+        else
+        {
+            int doubled = digit * 2;
+            sum2 += (doubled / 10) + (doubled % 10);
+        }
+
+        temp /= 10;
+        position++;
+    }
 }
