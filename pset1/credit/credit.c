b88f80b96c8e98eb6919741e430f2776ccbfa619
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-30 15:33:18 +0200

    Implement Luhn algorithm for credit card number validation

---
+++ pset1/credit/credit.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool luhn_check(long number);
+
+int main(void)
+{
+
+}
+
+bool luhn_check(long number)
+{
+    int sum = 0;
+    bool second = false;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+
+        if (second)
+        {
+            digit *= 2;
+
+            if (digit > 9)
+            {
+                digit -= 9;
+            }
+        }
+
+        sum += digit;
+        number /= 10;
+        second = !second;
+    }
+    return (sum % 10 == 0);
+}
