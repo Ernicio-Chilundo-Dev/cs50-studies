f1c534683a7def7056b11bd801a84c65c658f91b
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-30 21:23:33 +0200

    Add input validation loop for credit card number

--- pset1/credit/credit_more.c
+++ pset1/credit/credit_more.c
@@ -12,4 +12,18 @@ int main(void)
     }
     while (number < 1);
 
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user input 
+    long number;
+
+    do 
+    {
+        number = get_long("Card number: ");
+    }
+    while (number < 1);
+
 }
