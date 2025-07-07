99bd521a1a2823afe94a6814fc167270d2aa0cfd
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-07 06:47:14 +0200

    feat: implement readability checker using Coleman-Liau index

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -6,6 +6,49 @@
 
 int main(void)
 {
-    string text = get_string("Text: ")
-    
+    string text = get_string("Text: ");
+
+    int letters = 0, words = 1, sentences = 0;
+
+    // Count letters, words and sentences
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        char c = text[i];
+
+        if (c == c)
+        {
+            letters++;
+        }
+        else if (c == ' ')
+        {
+            words++;
+        }
+        else if (c == '.' || c == '!' || c == '?')
+        {
+            sentences++;
+        }
+    }
+
+    // calculete L and S
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+    // Coleman - Liau formula
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    // Round the index
+    int grade = round(index);
+
+    // Show the result
+    if (grade == 1)
+    {
+        printf("Before grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else{
+        printf("Grade %i\n", grade);
+    }
 }
