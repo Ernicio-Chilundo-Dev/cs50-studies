e6027881b8d62280bb315b8d275c8bf114e82837
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-11 11:42:55 +0200

    Implement recursive function to draw right-aligned staircase using hashes

--- pset3/iteration.c
+++ pset3/iteration.c
@@ -3,6 +3,24 @@
 
 void draw(int height);
 
+// int main(void)
+// {
+//     int height = get_int("Height: ");
+//     draw(height);
+// }
+
+// void draw(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < i + 1; j++)
+//         {
+//             printf("#");
+//         }
+//         printf("\n");
+//     }
+// }
+
 int main(void)
 {
     int height = get_int("Height: ");
@@ -11,12 +29,19 @@ int main(void)
 
 void draw(int n)
 {
+    
+    if (n <= 0)
+    {
+        return;
+    }
+    
+    draw(n - 1);
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        printf("#");
     }
+
+    printf("\n");
+
 }
