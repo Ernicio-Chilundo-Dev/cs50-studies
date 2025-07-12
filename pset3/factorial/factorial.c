3a2a5b72d3e01aea34fb48f8d3ac4a25a66fc9e9
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-12 17:09:23 +0200

    Implement recursive function to calculate factorial of a non-negative integer

---
+++ pset3/factorial/factorial.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int f(int n);
+
+int main(void)
+{
+    int number;
+
+    do 
+    {
+        number = get_int("Number: ");
+    } while (number < 0);
+
+    int result = f(number);
+    printf("Factorial of %i is %i\n", number, result);
+}
+
+int f(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * f(n - 1);
+    }
+}
