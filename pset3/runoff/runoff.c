52527c1ab67bc1fc486b22270452bf5449fce616
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-26 16:28:46 +0200

    Implement vote recording logic
    - Compare provided candidate name with registered candidates
    - If match is found, store candidate index in preferences array
    - Return true if vote is valid, false otherwise

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -107,4 +107,17 @@ int main(int argc, int argv[])
 }
 
 
+// Record preference if vote is valid
+bool vote(int voter, int rank, string name)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            preferences[voter][rank] = i;
+            return true;
+        }
+    }
+    return false;
+}
 
