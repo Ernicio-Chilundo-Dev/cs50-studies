fe2b1aed701c6a6fe24784dcbb13cac8f27ac670
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-26 15:34:05 +0200

    Initialize core data structures for runoff voting system
    - Define  2D array to store voter rankings
    - Create  struct to hold candidate data: name, votes, and elimination status
    - Declare  array with a fixed max size
    - Introduce global variables:  and
    
    These elements lay the foundation for implementing the runoff voting algorithm.

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -6,3 +6,23 @@
 #define MAX_VOTERS 100
 #define MAX_CANDIDATES 9
 
+// Preferences[i][j] iis jth  preference for vetor i
+
+int preferences[MAX_VOTERS][MAX_CANDIDATES];
+
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+} 
+candidate;
+
+candidate candidates[MAX_CANDIDATES];
+
+int voter_count;
+int candidate_count;
+
+
+
+
